7694182717b7ef7d87dbaf1dc742e92cd2731ee1
Vikash <115522144+Itzvikash022@users.noreply.github.com>
2024-09-22 19:34:06 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -9,9 +9,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors({
-    // origin: 'http://localhost:5173',
-    // credentials: true,
-}))
+    origin: 'https://nexify-chi.vercel.app' // Replace with your frontend domain
+  }));
 app.get('/', (req, res) =>{
     res.send('Hello World!')
 })
